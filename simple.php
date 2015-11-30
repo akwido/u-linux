1f872ac1211eb6f0faba77a1773992df723c191c
IZO <riektg@gmail.com>
2015-11-30 13:10:58 +0200

    save001

---
+++ simple.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>simple</title>
+   </head>
+<style>
+	body{background:#E6E6FA}
+</style>
+
+   <body>
+<?php
+
+	echo "--------git-------------";
+
+?>
+</body>
+</html>
